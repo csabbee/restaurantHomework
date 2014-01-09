a218fa7ae4271183e99dcce18d2218b20a47fe58
Csaba Valyi <csaba_valyi@epam.com>
2014-01-09 15:47:29 +0100

    init commit

---
+++ src/restaurant/Restaurant.java
@@ -0,0 +1,16 @@
+package restaurant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Restaurant {
+
+    private List<Cashier> cashiers = new ArrayList<>();
+    
+    public Cashier getCashier(){
+        
+        for (Cashier cashier : cashiers) {
+            
+        }
+    }
+}
