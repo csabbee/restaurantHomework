bc879c7164b13b74e6db69880086981f54525664
Csaba Valyi <csaba_valyi@epam.com>
2014-01-10 16:45:32 +0100

    building the "field"

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,12 @@
 package main;
+import restaurant.Cashier;
 import restaurant.Restaurant;
+import restaurant.Robot;
 import client.Client;
+import food.extra.concrete.Ketchup;
+import food.extra.concrete.Mustard;
+import food.product.concrete.Chips;
+import food.product.concrete.HotDog;
 
 
 public class Main {
@@ -8,7 +14,7 @@ public class Main {
     public static void main(String[] args) {
         Restaurant restaurant = new Restaurant();
         int clients = 0;
-        
+        init(restaurant);
         while(clients != 10){
             Thread t = new Thread(new Client(restaurant));
             t.start();
@@ -17,7 +23,20 @@ public class Main {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            clients++;
+            Robot.getInstance().makeNextOrder();
         }
+    }
+
+    
+    private static void init(Restaurant restaurant) {
+        restaurant.addCashier(new Cashier());
+        restaurant.addCashier(new Cashier());
+        restaurant.addCashier(new Cashier());
+        restaurant.addNewExtra(new Ketchup());
+        restaurant.addNewExtra(new Mustard());
+        restaurant.addNewProduct(new Chips());
+        restaurant.addNewProduct(new HotDog());
     }
 
 }
