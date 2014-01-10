dac7cacce8b55695781dd70e481e725554fd9691
Csaba Valyi <csaba_valyi@epam.com>
2014-01-10 13:32:11 +0100

    Manager is now a singleton

--- src/restaurant/Manager.java
+++ src/restaurant/Manager.java
@@ -1,6 +1,21 @@
 package restaurant;
 
 public class Manager {
+    private static final Manager INSTANCE;
 
-    
+    static {
+        INSTANCE = new Manager();
+    }
+
+    private Manager() {
+        
+    }
+
+    public static Manager getInstance() {
+        return INSTANCE;
+    }
+
+    public static void sfoo() {
+        
+    }
 }
