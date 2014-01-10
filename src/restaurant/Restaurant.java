83423a6dcc8a0b15e6cd02fb3a1d9977d68aea17
Csaba Valyi <csaba_valyi@epam.com>
2014-01-10 13:31:58 +0100

    get and addCashier functions

--- src/restaurant/Restaurant.java
+++ src/restaurant/Restaurant.java
@@ -3,14 +3,28 @@ package restaurant;
 import java.util.ArrayList;
 import java.util.List;
 
+import food.extra.Extra;
+import food.product.Product;
+
 public class Restaurant {
 
     private List<Cashier> cashiers = new ArrayList<>();
+    private List<Product> products = new ArrayList<>();
+    private List<Extra> extras = new ArrayList<>();
     
     public Cashier getCashier(){
-        
-        for (Cashier cashier : cashiers) {
-            
+        Cashier cashier = null;
+        if(!cashiers.isEmpty()){
+            synchronized (cashiers){
+                if(!cashiers.isEmpty()){
+                    cashier = cashiers.remove(cashiers.size()-1);
+                }
+            }
         }
+        return cashier;
+    }
+    
+    public void addCashier(Cashier cashier){
+        cashiers.add(cashier);
     }
 }
