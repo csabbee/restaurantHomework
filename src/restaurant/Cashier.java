587cf51273c34da47e3e175ae136252651b63162
Csaba Valyi <csaba_valyi@epam.com>
2014-01-10 16:45:09 +0100

    basic functionality for the cashier

--- src/restaurant/Cashier.java
+++ src/restaurant/Cashier.java
@@ -1,6 +1,38 @@
 package restaurant;
 
+import client.Client;
+import food.product.Product;
+
 public class Cashier {
 
+    private Order order;
+    public Cashier(){
+        order = new Order(this);
+    }
+    
+    public void deleteOrder() {
+        order = new Order(this);
+    }
+
+    public void modifyOrder(Product product){
+        order.addProduct(product);
+    }
+
+    public void startOrdering(Client client) {
+        System.out.format("%s is here%n", client.toString());
+        order.setClient(client);
+    }
+
+    public void finishOrder() {
+        Robot.getInstance().addOrder(order);
+        //deleteOrder();
+    }
+
+    public void orderFinished() {
+        System.out.format("%s ", order.toString());
+        System.out.format("%s%n", order.getClient().toString());
+        order.getClient().orderFinished(order);
+    }
+    
     
 }
