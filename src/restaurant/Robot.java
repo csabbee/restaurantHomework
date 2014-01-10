ae58aece75945575faf52756c6fbb857fe25a8a7
Csaba Valyi <csaba_valyi@epam.com>
2014-01-10 16:43:25 +0100

    Robot class got a queue and some other stuff

--- src/restaurant/Robot.java
+++ src/restaurant/Robot.java
@@ -1,6 +1,31 @@
 package restaurant;
 
-public class Robot {
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
+
+public class Robot{
+    private static final Robot INSTANCE;
+    private static Queue<Order> orderQueue; 
     
+    static {
+        INSTANCE = new Robot();
+        orderQueue = new ConcurrentLinkedQueue<>();
+    }
+
+    private Robot() {}
+
+    public static Robot getInstance() {
+        return INSTANCE;
+    }
+
+    public void addOrder(Order order) {
+        orderQueue.add(order);
+    }
+
+    public void makeNextOrder() {
+        if(orderQueue.size() > 0){
+            orderQueue.poll().getCashier().orderFinished();
+        }
+    }
 }
